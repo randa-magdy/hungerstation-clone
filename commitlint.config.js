611722808e356f866c1a7efb26eac3b8dbc20ea6
randa-magdy <randamagdy123@gmail.com>
2024-06-10 13:03:39 +0300

    chore: updating some of dependencies & configurations

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,45 +7,43 @@
 // refactor: A code change that neither fixes a bug nor adds a feature
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
+//chore: Updating dependencies, configurations, or build-related files.(not change the functionality of the code)
+//revert:Fix issue with authentication
 
 module.exports = {
-    // extends: ['@commitlint/config-conventional'],
-    extends:[],
-    rules: {
-      'body-leading-blank': [1, 'always'],
-      'body-max-line-length': [2, 'always', 100],
-      'footer-leading-blank': [1, 'always'],
-      'footer-max-line-length': [2, 'always', 100],
-      'header-max-length': [2, 'always', 100],
-      'scope-case': [2, 'always', 'lower-case'],
-      'subject-case': [
-        2,
-        'never',
-        ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+  // extends: ['@commitlint/config-conventional'],
+  extends: [],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'translation',
+        'security',
+        'changeset',
       ],
-      'subject-empty': [2, 'never'],
-      'subject-full-stop': [2, 'never', '.'],
-      'type-case': [2, 'always', 'lower-case'],
-      'type-empty': [2, 'never'],
-      'type-enum': [
-        2,
-        'always',
-        [
-          'build',
-          'chore',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'test',
-          'translation',
-          'security',
-          'changeset',
-        ],
-      ]
-    },
-  };
+    ],
+  },
+};
